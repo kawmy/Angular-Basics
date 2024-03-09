406c22414b57c45c66d880370346d25456903cd2
Kamyar mofakhami <41609894+kawmy@users.noreply.github.com>
2024-03-10 03:17:16 +0330

    Add post component and update app component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
 
+export class PostComponent {
+  @Input() postImgUrl = '';
 }
